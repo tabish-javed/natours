e1394db3056e6355646557230ddd3f8925e5c22f
TABISH <tabishjaved@hotmail.com>
2023-08-31 17:23:14 +0530

    setup of first get route using async callback

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const fs = require('node:fs/promises');
 const express = require('express');
 const app = express();
 
+/*
 app.get('/', (request, response) => {
     response
         .status(200)
@@ -11,6 +13,24 @@ app.post('/', (request, response) => {
     response
         .status(200)
         .send('You can post to this endpoint...');
+});
+*/
+
+
+
+
+
+app.get('/api/v1/tours', async (request, response) => {
+
+    const rawData = await fs.readFile(`${__dirname}/dev-data/data/tours-simple.json`);
+    const tours = await JSON.parse(rawData);
+
+    response.status(200).json({
+        status: 'success',
+        data: {
+            tours
+        }
+    });
 });
 
 
