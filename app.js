9127eb56247564d1de5e5b558f40186de2c8363c
TABISH <tabishjaved@hotmail.com>
2023-08-31 16:58:07 +0530

    initial of Notours API App

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    response
+        .status(200)
+        .json({ message: 'Hello from the server side!', app: 'Natorurs' });
+});
+
+app.post('/', (request, response) => {
+    response
+        .status(200)
+        .send('You can post to this endpoint...');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+});
