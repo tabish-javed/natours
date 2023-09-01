ab31e96c0334bb1f3de9eb118c19409549c6474d
TABISH <tabishjaved@hotmail.com>
2023-09-01 16:16:58 +0530

    two basic get and post routes created

--- app.js
+++ app.js
@@ -16,19 +16,41 @@ app.post('/', (request, response) => {
 });
 */
 
+async function readJSONFile () {
+    return await JSON.parse(await fs.readFile(`${__dirname}/dev-data/data/tours-simple.json`));
+}
+
+async function writeJSONFile (data) {
+    await fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(data, null, 4));
+}
 
 
 
+app.use(express.json());
 
 app.get('/api/v1/tours', async (request, response) => {
-
-    const rawData = await fs.readFile(`${__dirname}/dev-data/data/tours-simple.json`);
-    const tours = await JSON.parse(rawData);
-
+    const tours = await readJSONFile();
     response.status(200).json({
         status: 'success',
+        results: tours.length,
         data: {
-            tours
+            tours: tours
+        }
+    });
+});
+
+
+app.post('/api/v1/tours', async (request, response) => {
+    const tours = await readJSONFile();
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newID }, request.body);
+    tours.push(newTour);
+    // JSON.stringify with these options return JSON text in human readable format.
+    await writeJSONFile(tours);
+    response.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour
         }
     });
 });
