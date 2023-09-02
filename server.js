21ea560c5d4c9641c32af5a7ed8feaa0370c5623
TABISH <tabishjaved@hotmail.com>
2023-09-02 23:20:27 +0530

    added eslint and graceful server shutdown

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // INTERNAL
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
@@ -6,6 +7,13 @@ const app = require('./app');
 
 // Server Startup
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`API server started on port: ${port}`);
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+    });
 });
