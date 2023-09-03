1c2a3b85e2a714368cd29713d5c63eb9b657a238
TABISH <tabishjaved@hotmail.com>
2023-09-03 14:20:14 +0530

    tourSchema created and used in creating Tour model

--- server.js
+++ server.js
@@ -7,13 +7,32 @@ const mongoose = require('mongoose');
 const app = require('./app');
 //---------------------------------------------------
 
-
+// mongoDB Connection Setup
 const DB_URL = process.env.DB_CONNECT_STRING
     .replace('<USERNAME>', process.env.DB_USERNAME)
     .replace('<PASSWORD>', process.env.DB_PASSWORD)
     .replace('<DB>', 'natours');
 
 mongoose.connect(DB_URL).then(() => console.log('Successfully connected to Database...'));
+
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 
 // Server Startup
