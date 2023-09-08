ba44f4674f20c5dc4b275335eb085d9c7ed6b58d
TABISH <tabishjaved@hotmail.com>
2023-09-08 22:32:55 +0530

    added error handling middleware on bottom

--- app.js
+++ app.js
@@ -34,9 +34,28 @@ app.use('/api/v1/users', userRouter);
 
 // bottom middleware 2 - custom (handling all other URLs)
 app.all('*', function (request, response, next) {
-    response.status(404).json({
-        status: 'failed',
-        message: `Can't find ${request.url} on this server!`
+    // response.status(404).json({
+    //     status: 'failed',
+    //     message: `Can't find ${request.url} on this server!`
+    // });
+    // next();
+
+    const error = new Error(`Can't find ${request.url} on this server!`);
+    error.status = 'failed';
+    error.statusCode = 404;
+
+    next(error);
+});
+
+
+// error handling middleware
+app.use((error, request, response, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+
+    response.status(error.statusCode).json({
+        status: error.status,
+        message: error.message
     });
     next();
 });
