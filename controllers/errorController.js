647b3f65c346398f76662bdfc48e3ba9a6e80186
TABISH <tabishjaved@hotmail.com>
2023-09-09 22:58:30 +0530

    handled invalid database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,11 @@
+const AppError = require('./../utils/appError');
+
+function handleCastErrorDB (err) {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+}
+
+
 function sendErrorDev (error, response) {
     response.status(error.statusCode).json({
         status: error.status,
@@ -17,7 +25,7 @@ function sendErrorPrd (error, response) {
     } else {
         // Programming or other unknown errors: don't leak error details
         // 1 - log Error
-        console.error('ERROR! - ', error);
+        // console.error('ERROR! - ', error);
         // 2 - send error
         response.status(500).json({
             status: 'error',
@@ -29,14 +37,15 @@ function sendErrorPrd (error, response) {
 
 function globalErrorHandler (error, request, response, next) {
     // console.log(error.stack);
-
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(error, response);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorPrd(error, response);
+        let err = JSON.parse(JSON.stringify(error));
+        if (err.name === 'CastError') err = handleCastErrorDB(err);
+        sendErrorPrd(err, response);
     }
 
     next();
