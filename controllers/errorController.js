b38e32c69028a882fd22e90956fdf2133d91b690
TABISH <tabishjaved@hotmail.com>
2023-09-09 19:44:07 +0530

    separate error handling - DEV & PRD environments

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,44 @@
+function sendErrorDev (error, response) {
+    response.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        error: error,
+        stack: error.stack
+    });
+}
+
+function sendErrorPrd (error, response) {
+    if (error.isOperational) {
+        // Operational and trusted error: send message to client
+        response.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+        });
+    } else {
+        // Programming or other unknown errors: don't leak error details
+        // 1 - log Error
+        console.error('ERROR! - ', error);
+        // 2 - send error
+        response.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        });
+    }
+}
+
+
 function globalErrorHandler (error, request, response, next) {
     // console.log(error.stack);
 
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
 
-    response.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(error, response);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorPrd(error, response);
+    }
+
     next();
 };
 
