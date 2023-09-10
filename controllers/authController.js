3d62ec50612b886ab0ff9a3693fb65dbbe214d5c
TABISH <tabishjaved@hotmail.com>
2023-09-10 15:33:58 +0530

    create new user complete

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+const User = require('./../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+
+const signUp = catchAsync(async function (request, response, next) {
+    const newUser = await User.create(request.body);
+
+    response.status(201).json({
+        status: 'success',
+        data: {
+            user: newUser
+        }
+    });
+});
+
+
+module.exports = {
+    signUp: signUp
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,14 @@
 const express = require('express');
 // CUSTOM
 const userController = require('./../controllers/userController');
+const authController = require('../controllers/authController');
 
 
 const router = express.Router();
+
+
+router.post('/signup', authController.signUp);
+
 router.route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser);
