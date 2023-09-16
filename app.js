de9043af5e9cc49db1df66ca2ced3f47b90166e4
TABISH <tabishjaved@hotmail.com>
2023-09-16 22:01:12 +0530

    global rateLimiter middleware added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // EXTERNAL
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 // CUSTOM
 const AppError = require('./utils/appError');
 const errorController = require('./controllers/errorController');
@@ -12,16 +13,28 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// GLOBAL MIDDLEWARES
 // third-party middleware to enable logging (only in development environment)
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// express middleware  - to receive jason.parse()ed JS-Object from request's body (which is in JSON)
+// express global middleware - to limit number of request from specific IP
+const rateLimitOptions = {
+    max: 100,
+    windowMs: 60 * 60 * 1_000,
+    message: "Too many request from this IP, please try again in an hour!"
+};
+app.use('/api', rateLimit(rateLimitOptions));
+
+
+// express global middleware - to receive jason.parse()ed JS-Object from request's body (which is in JSON)
 app.use(express.json());
 
-// middleware for public files
+
+// express global middleware - serves public files
 app.use(express.static(`${__dirname}/public`));
+
 
 // middleware 1 - custom
 app.use((request, response, next) => {
