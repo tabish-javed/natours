0e131e49d24037e56760e7db33741412f405e13d
TABISH <tabishjaved@hotmail.com>
2023-09-21 16:14:27 +0530

    modeling tour guides, using embedding!

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,7 @@
 // EXTERNAL
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // require('validator');
 
 // define schema for tours documents inside database
@@ -109,7 +110,10 @@ const tourSchema = new mongoose.Schema({
             description: String,
             day: Number
         }
-    ]
+    ],
+    guides: {
+        type: Array
+    }
 }, {
     toJSON: { virtuals: true },     // enable virtual properties/fields
     toObject: { virtuals: true }    // enable virtual properties/fields
@@ -124,6 +128,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() but not for .insertMany()
 tourSchema.pre('save', function (next) {
     this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+tourSchema.pre('save', async function (next) {
+    const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+    this.guides = await Promise.all(guidesPromises);
     next();
 });
 
