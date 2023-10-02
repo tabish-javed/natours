33027cef879f30427fae322aff57e57845e36710
TABISH <tabishjaved@hotmail.com>
2023-10-02 16:12:37 +0530

    calculate average rating and saving to Tour doc

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import Tour from '../models/tourModel.js';
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -27,16 +27,48 @@ const reviewSchema = new mongoose.Schema({
     }
 }, {
     toJSON: { virtuals: true },     // enable virtual properties/fields
-    toObject: { virtuals: true }    // enable virtual properties/fields
+    toObject: { virtuals: true },    // enable virtual properties/fields
 });
 
 
 reviewSchema.pre(/^find/, function (next) {
+    // "this" points to the current query
     this.populate({
         path: 'user',
         select: 'name photo'
     });
     next();
+});
+
+
+reviewSchema.statics.calcAverageRatings = async function (tourID) {
+    // "this" points to current model
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourID }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+
+    await Tour.findByIdAndUpdate(tourID, {
+        ratingsQuantity: stats[0].nRating,
+        ratingsAverage: stats[0].avgRating
+    });
+
+};
+
+
+reviewSchema.post('save', function () {
+    // "this" points to the current review/document
+    // "this.constructor" points to current model who created the document
+    // (we can't use Review as it's not yet declared)
+    this.constructor.calcAverageRatings(this.tour);
 });
 
 
